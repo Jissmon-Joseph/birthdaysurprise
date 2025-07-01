3e9908e6582c5bc41d39b56596e2171b8c9d6795
Jissmon Joseph <jissmonjoseph10@gmail.com>
2025-07-01 14:10:59 +0530

    homecompleted

--- Controller/brithdayController.js
+++ Controller/brithdayController.js
@@ -1,4 +1,10 @@
-
+const loadEntrancePage = (req, res) => {
+    try {
+        res.render("entrance")
+    } catch (err) {
+        console.log("Error in load entrance:", err);
+    }
+} 
 const loadGiftBoxPage = (req, res) => {
     try {
         res.render("giftbox");
--- public/js/entrance.js
+++ public/js/entrance.js
@@ -2,9 +2,9 @@ const heroSection = document.querySelector(".hero-section");
 
 // 3 background images
 const backgrounds = [
-  "https://images.unsplash.com/photo-1503264116251-35a269479413", // image 1
-  "https://images.unsplash.com/photo-1529626455594-4ff0802cfb7e", // image 2
-  "https://images.unsplash.com/photo-1535713875002-d1d0cf377fde", // image 3
+  "/asset/entranceimages/IMG_20250630_164102-Picsart-AiImageEnhancer.jpg", // image 1
+  "/asset/entranceimages/IMG_20250630_164341-Picsart-AiImageEnhancer.jpg", // image 2
+  "/asset/entranceimages/IMG_20250630_173945-Picsart-AiImageEnhancer.jpg", // image 3
 ];
 
 let currentIndex = 0;
